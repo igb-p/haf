15665f63f6fc423b1ac684b60aa9b52a86c27fa4
igb-p <dima12p@gmail.com>
2022-03-26 12:49:47 +0700

    \

--- .vscode/main.cpp
+++ .vscode/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 int main() 
 {
-cout <<"Hello";
+    int aski[256], x;
+    for(int i=0; i<256; i++) aski[i]=0;
+    ifstream a("text.txt");
+    if(!a){cout<<"Ошибка открытия файла"; return 0;};
+    while(a){
+        a>>x; aski[x]++;
+    }
+    a.close();
 }
